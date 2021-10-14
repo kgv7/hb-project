1ab6656b5f0696165e01576d02ec423171c8c6bf
Kaitlyn <kaitlyn.a.goodman@gmail.com>
2021-10-14 04:01:27 +0000

    working on seeding database

--- seed_database.py
+++ seed_database.py
@@ -1,4 +1,4 @@
-"""Seeds the database with users, electric vehicles, charging stations, reviews, ."""
+"""Seeds the database with users, electric vehicles, charging stations, reviews."""
 
 import os, json
 from random import choice, randint
@@ -11,5 +11,37 @@ os.system('createdb evchargers')
 
 model.connect_to_db(server.app)
 model.db.create_all()
+
+fake = Faker()
+
+
+
+# Load charging level data from JSON file
+with open('data/charging-level.json') as f:
+    charging_level_data = json.loads(f.read())
+
+# create users
+# user_db = []
+# for i in range(10):
+#     fname = fake.first_name()
+#     lname = fake.last_name()
+#     email = fake.email()
+#     password = "password123"
+#     ev_id = i
+#     new_user = crud.create_user(fname, lname, email, password, ev_id)
+#     user_db.append(new_user)
+
+# create EVs from json
+    # Load ev data from JSON file
+with open('data/ev.json') as f:
+    data = json.loads(f.read())
+    ev_data = data[result]
+
+ev_db = []
+for ev in ev_data:
+    model = ev[model]
+    print(model)
+    model_year = ev[model_year]
+    print(model_year)
 
 
