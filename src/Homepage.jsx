105d72860f061e98cb188d73d831d9cf765a1ea2
kgv7 <kaitlyn.a.goodman@gmail.com>
2021-12-05 03:23:52 +0000

    working on styling homepage

--- src/Homepage.jsx
+++ src/Homepage.jsx
@@ -7,38 +7,39 @@ export default function Homepage(props) {
       // <div className="container">
       <div>
         <div className="row position-relative overflow-hidden p-3 p-md-5 m-md-3 text-center" id="home-banner">
-        {/* <div 
-        className="position-relative overflow-hidden p-3 p-md-5 m-md-3 text-center"
-        > */}
-          <div id="logo-header"><img src="./img/recharge-logo-white.png"/></div>
-          <p className="lead">Fuel your car and your stomach</p>
-        {/* </div> */}
+          <div id="logo-header"><img src="./img/recharge-logo-white.png" class="img-fluid"/></div>
+          <p className="tagline">Fuel your car and your stomach</p>
+    
         </div>
 
+      <section id="steps">
+      <h1 className="steps-header">How It Works</h1>
         <div className="row d-md-flex flex-md-equal w-100 my-md-3 ps-md-3">
-        {/* <div className="d-md-flex flex-md-equal w-100 my-md-3 ps-md-3"> */}
-        <div className="card-deck">
-            <div className="card ">
-              <div className="card-body">
-                <h5 className="card-title">Step One: Find a Charger</h5>
-                <p className="card-text">Use Google Maps to find a charger at your destination</p>
+          <div className="col step-home">
+
+                <i class="fas fa-charging-station fa-3x"></i>
+                <h5 className="step-title">Step One:<p>Find a Charger</p></h5>
+                <div className="step-text">Find a charger at your destination</div>
               </div>
-            </div>
-            <div className="card">
-              <div className="card-body">
-                <h5 className="card-title">Step Two: Calculate Charge Time</h5>
-                <p className="card-text">Find out how long it will take to charge your car</p>
+          <div className="col step-home">
+
+                <i class="fas fa-calculator fa-3x"></i>
+                <h5 className="step-title">Step Two:
+                <p>Calculate Charge Time</p></h5>
+                <div className="step-text">Calculate out how long it will take to charge your car</div>
               </div>
-            </div>
-            <div className="card">
-              <div className="card-body">
-                <h5 className="card-title">Step Three: Find Walkable Restaurants</h5>
-                <p className="card-text">Your charge time may be long, so find some walkable restaurants near the charger you select.</p>
+  
+              <div className="col step-home">
+ 
+              <i class="fas fa-utensils fa-3x"></i>
+                <h5 className="step-title">Step Three:<p>Choose a Restaurant</p></h5>
+                <div className="step-text">Choose a restaurant that's within walking distance while your car is charging.</div>
               </div>
-            </div>
+              </div>
+      
+          <div className="get-started-btn"><Link to="/find-charger"><button className="btn btn-outline-secondary">Get Started</button></Link></div>
+          </section>
+
           </div>
-          </div><Link to="/find-charger"><button>Get Started</button></Link>
-          </div>
-          // </div>
     );
   }
--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -11,9 +11,7 @@ export default function Navbar(props) {
 
     if (!token) {
       return (
-        <nav>
-    
-          <section className="d-flex justify-content-center">
+        <nav className="navbar navbar-expand-lg navbar-light pt-4">
           <Link
             to="/"
             className="navbar-brand d-flex justify-content-center"
@@ -21,36 +19,56 @@ export default function Navbar(props) {
             <img src={logo} height="30" />
           </Link>
 
-            <NavLink
-              to="/find-charger"
-              activeClassName="navlink-active"
-              className="nav-link nav-item"
-            >
-              Find Charger
-            </NavLink>
-            <NavLink
-              to="/add-station"
-              activeClassName="navlink-active"
-              className="nav-link nav-item"
-            >
-              Add Station
-            </NavLink>
-            <NavLink
-              to="/about-charging"
-              activeClassName="navlink-active"
-              className="nav-link nav-item"
-            >
-              About
-            </NavLink>
-            <NavLink
-                to="/account"
+          <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#rechargeNavbar" aria-controls="rechargeNavbar" aria-expanded="false" aria-label="Toggle navigation">
+            <span className="navbar-toggler-icon"></span>
+          </button>
+            <div class="collapse navbar-collapse" id="rechargeNavbar">
+            
+            <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+              <li className="nav-item">
+                <NavLink
+                to="/find-charger"
                 activeClassName="navlink-active"
-                className="nav-link nav-item"
-              >
-                <button className="btn btn-outline-secondary">Login/Register</button>
-               
-              </NavLink>
-              </section></nav>
+                className="nav-link"
+                >
+                  Find Charger
+                </NavLink>
+              </li>
+            
+              <li className="nav-item">
+                <NavLink
+                  to="/add-station"
+                  activeClassName="navlink-active"
+                  className="nav-link"
+                >
+                  Add Station
+                </NavLink>
+              </li>
+
+              <li className="nav-item">
+                <NavLink
+                  to="/about-charging"
+                  activeClassName="navlink-active"
+                  className="nav-link"
+                >
+                  About
+                </NavLink>
+              </li>
+              </ul>
+              <span className="nav-item">
+                <NavLink
+                    to="/account"
+                    activeClassName="navlink-active"
+                    className="nav-link"
+                  >
+                    <button className="btn btn-outline-secondary">Login/Register</button>
+                  
+                  </NavLink>
+              </span>
+
+
+              </div>
+        </nav>
           )
 
     } else {
@@ -87,13 +105,25 @@ export default function Navbar(props) {
             >
               About
       </NavLink>
-      <NavLink
+      {/* <NavLink
         to="/profile"
         activeClassName="navlink-active"
         className="nav-link nav-item"
       >
         Profile
-      </NavLink>
+      </NavLink> */}
+
+      <div className="nav-item">
+                <NavLink
+                    to="/profile"
+                    activeClassName="navlink-active"
+                    className="nav-link nav-item "
+                  >
+                    <button className="btn btn-outline-secondary">Profile</button>
+                  
+                  </NavLink>
+              </div>
+
         </section>
       </nav>)
   };
